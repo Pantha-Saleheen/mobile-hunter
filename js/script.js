6b91a1d223106ac2835766efab922a5eb50b5d8e
Muktadir-Us-Saleheen <saleheen110282@gmail.com>
2022-03-02 23:23:42 +0600

    search function created

--- js/script.js
+++ js/script.js
@@ -7,3 +7,28 @@ const notFoundMessage = document.getElementById('not-found');
 clickedItem.style.display = 'none';
 warningMessage.style.display = 'none';
 notFoundMessage.style.display = 'none';
+
+
+
+//search mobile function
+const searchMobile = () =>{
+    const searchText = document.getElementById('search-field');
+    const searchTextValue = searchText.value;
+    searchText.value = '';
+
+    //for empty input field
+        if(searchTextValue == ''){
+            notFoundMessage.style.display = 'none';
+            warningMessage.style.display = 'block';
+            warningMessage.innerText = 'Please enter a value in search box!!';
+            document.getElementById('search-result').textContent = '';
+        }
+        else{
+            warningMessage.style.display = 'none';
+            const url = `https://openapi.programming-hero.com/api/phones?search=${searchTextValue}`;
+
+            fetch(url)
+            .then(res => res.json())
+            .then(data => displayResult(data.data));
+        }
+}
