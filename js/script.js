357ea1512fd1c4bd452e3c6f434209dd8279b546
Muktadir-Us-Saleheen <saleheen110282@gmail.com>
2022-03-02 23:22:34 +0600

    global declaration and display none

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Global declaration
+const clickedItem = document.getElementById('clicked');
+const warningMessage = document.getElementById('warning');
+const notFoundMessage = document.getElementById('not-found');
+
+// display none 
+clickedItem.style.display = 'none';
+warningMessage.style.display = 'none';
+notFoundMessage.style.display = 'none';
